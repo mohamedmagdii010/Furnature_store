b47bbe4589a5c2eb6524d1bc2fde67288c1be360
mohamedmagdii010 <mohamedmagdii659@gmail.com>
2023-07-11 17:09:22 +0300

    first commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,50 +22,59 @@ export class ContactComponent {
   }
   titi(){
   
-   if(this.titlestyle === 'none'){
-  
-    this.titlestyle = 'display:block'
-   }else{
-  
-    this.titlestyle = 'display:none'
-  
-   }
+    if (this.titlestyle === 'none'|| this.titlestyle === "display:none" ){
+
+      console.log("ddd", this.titlestyle)
+        this.titlestyle = "display:grid"
+      }else{
+      
+        console.log("ssssss", this.titlestyle)
+        this.titlestyle = "display:none";
+      
+      }
+      
   
   }
 
   hey(){
-    if(this.titlestyle1 === 'nonee'){
-  
-      this.titlestyle1 = 'display:block'
-     }else{
-    
-      this.titlestyle1 = 'display:none'
-    
-     }
+    if (this.titlestyle1 === 'none'|| this.titlestyle1 === "display:none" ){
+
+      console.log("ddd", this.titlestyle1)
+        this.titlestyle1 = "display:grid"
+      }else{
+      
+        console.log("ssssss", this.titlestyle1)
+        this.titlestyle1 = "display:none";
+      
+      }
 
   }
 
   bay(){
 
-    if(this.titlestyle2 === 'nonee'){
-  
-      this.titlestyle2 = 'display:block'
-     }else{
-    
-      this.titlestyle2 = 'display:none'
-    
-     }
+    if (this.titlestyle2 === 'none'|| this.titlestyle2 === "display:none" ){
+
+      console.log("ddd", this.titlestyle2)
+        this.titlestyle2 = "display:grid"
+      }else{
+      
+        console.log("ssssss", this.titlestyle2)
+        this.titlestyle2 = "display:none";
+      
+      }
   }
   bay2(){
 
-    if(this.titlestyle3 === 'nonee'){
-  
-      this.titlestyle3 = 'display:block'
-     }else{
-    
-      this.titlestyle3 = 'display:none'
-    
-     }
+    if (this.titlestyle3 === 'none'|| this.titlestyle3 === "display:none" ){
+
+      console.log("ddd", this.titlestyle3)
+        this.titlestyle3 = "display:grid"
+      }else{
+      
+        console.log("ssssss", this.titlestyle3)
+        this.titlestyle3 = "display:none";
+      
+      }
   }
   
   
